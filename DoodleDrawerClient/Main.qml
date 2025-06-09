bb79991eca38d7d7e81ad929206684e9dd689d87
Rmsae <156903870+Rmsae@users.noreply.github.com>
2025-06-09 19:05:52 +0200

    Client Side Web Socket

--- DoodleDrawerClient/Main.qml
+++ DoodleDrawerClient/Main.qml
@@ -1,8 +1,15 @@
 import QtQuick
+import QtQuick.Controls
 
 Window {
     width: 640
     height: 480
     visible: true
     title: qsTr("Hello World")
+
+    Button {
+        anchors.centerIn: parent
+        text: "Click Me!"
+        onClicked: webSocketHandler.connectToServer("www.google.com")
+    }
 }
--- DoodleDrawerClient/main.cpp
+++ DoodleDrawerClient/main.cpp
@@ -1,9 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "Controllers/websockethandler.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    WebSocketHandler socketHandler;
 
     QQmlApplicationEngine engine;
     QObject::connect(
@@ -12,6 +17,9 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
+    QQmlContext *context = engine.rootContext();
+    context->setContextProperty("webSocketHandler", &socketHandler);
     engine.loadFromModule("DoodleDrawerClient", "Main");
 
     return app.exec();
